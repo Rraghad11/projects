f1a4addac307fad7a9b0f753070866f511214a34
raghad altrisy <157610120+Rraghad11@users.noreply.github.com>
2024-09-25 10:45:01 +0300

    Add files via upload

---
+++ LibraryProjects/apps/usermodoles/views.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('add/', views.add_book, name='add_book'),
+]
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from .forms import BookForm
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'book_detail.html', {'book': book})
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
